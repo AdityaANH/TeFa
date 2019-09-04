00eb48b2d0eb36acdfda362e676d93caf12e594a
aditya <aditya.n.h02@gmail.com>
2019-09-04 20:41:07 +0700

    TeFa-heroku

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const port = process.env.PORT || 9000;
+
+http.createServer(function(req, res){
+    res.writeHead(200,{'content-type' : 'text/plain'});
+    res.end('{ "success":true, "data":"Index", "message": "Hello World", "code":200}');
+}).listen(port,'127.0.0.1');
