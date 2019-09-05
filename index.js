1c621c74a81156dbb49c7c55048901aa622686f9
aditya <aditya.n.h02@gmail.com>
2019-09-05 07:08:27 +0700

    add procfile

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 9000;
+const port = 9000;
 
 http.createServer(function(req, res){
     res.writeHead(200,{'content-type' : 'text/plain'});
